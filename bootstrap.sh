34166501270f411abb80a3e988eacc6fe28798bf
Adam Pacholski <adam.pacholski@rover.com>
2022-05-04 10:02:01 -0700

    Try .bashrc instead of .bash_profile as target for dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # Create symlink for .bash_profile
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.bash_profile /home/vscode/.bash_profile
-
-# Load .bash_profile
-source /home/vscode/.bash_profile
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.bash_profile /home/vscode/.bashrc
