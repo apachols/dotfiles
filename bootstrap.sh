001564d8baaebc566201e6c5c0153fc4e325e364
Adam Pacholski <adam.pacholski@rover.com>
2022-05-11 06:05:41 -0700

    Add personal gitconfig to codespaces setup routine

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,5 @@
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.bash_profile /home/vscode/.bash_profile
 
 echo "source /home/vscode/.bash_profile" >> /home/vscode/.bashrc
+
+cat /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig >> /home/vscode/.gitconfig
