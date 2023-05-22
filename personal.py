c005feff9bf86b2e0c152280e36bb1262b695936
Adam Pacholski <adam.pacholski@rover.com>
2023-05-22 11:46:32 -0700

    Add personal.py

---
+++ personal.py
@@ -0,0 +1,58 @@
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# CELERY_TASK_ALWAYS_EAGER - default setting is True
+#
+# IF TRUE, run all tasks immediately, IN WEB CONTAINER
+#
+# IF FALSE, run delayed tasks async, IN THE WORKER CONTAINER
+#
+# NOTE - in order to send megatron_streaming events to kafka locally,
+#        CELERY_TASK_ALWAYS_EAGER must be True
+#
+# NOTE - Strange errors often occur when CELERY_TASK_ALWAYS_EAGER is True.
+#        For most cases prefer _____ CELERY_TASK_ALWAYS_EAGER = FALSE _____
+#
+#
+CELERY_TASK_ALWAYS_EAGER = True
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
+MEGATRON_TASKS_ENABLED = True
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+
+# import os
+# PTVSD_DEBUG = os.environ.get('PTVSD_DEBUG') == "1"
+
+# Needed for developing metrics / dashboards / monitors locally
+# https://roverdotcom.atlassian.net/wiki/spaces/TECH/pages/1120964125/Local+Development+of+Metrics+Dashboards+and+Monitors
+
+# Use this to debug local statsd metrics to Rover.com-Dev organization
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# THREADSTATSD_DEVELOPER_TAG = 'adamp'
+# STATSD_CLIENT = 'systems.datadog.statsd.ThreadStatsD'
+
+# Use this to print out statsd metrics locally
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# STATSD_CLIENT = 'systems.statsd.pystatsd.PyStatsD'
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'systems.statsd.pystatsd': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': False,
+#         },
+#     }
+# }
